e34fa6a06f0208359db4769b6255612f4ed630a0
moonman <74813604+abhignay@users.noreply.github.com>
2024-02-11 11:05:44 +0530

    added vehicle command section

--- GCS-v2.py
+++ GCS-v2.py
@@ -16,24 +16,26 @@ class GUI(QWidget):
 
         # runs window setup, where the main gui window is initialized and all other functions are run
         self.window_setup() 
-
+        self.hello =0
         # timer 
         self.timer = QtCore.QTimer()  # type: ignore
-        self.timer.setInterval(300)
+        self.timer.setInterval(400)
 
         self.timer.timeout.connect(self.breakData)
         self.timer.timeout.connect(self.updateData)
         self.timer.timeout.connect(self.state_Box)
         self.timer.timeout.connect(self.updateDateTime)
+        self.timer.timeout.connect(self.CamButtons)
 
-        # updates data in every 350 milliseconds
+        # updates data in every 400 milliseconds
         self.timer.start()
+
         
     def window_setup(self):
         # window setup
         self.setStyleSheet("background-color: gray;" "QLabel{font-size: 24pt;}")
         self.setWindowTitle("TFAC Ground Control v2")
-        self.setGeometry(0, 0, 525, 486)
+        self.setGeometry(0, 0, 525, 784)
             
         # run the other functions
         self.time_box()
@@ -41,7 +43,9 @@ class GUI(QWidget):
         self.TFAC_times()
         self.stateInit()
         self.main_TLMBox()
+        self.commandBox()
         self.mainData() 
+        self.CommandButtons()
         self.dataSeparator()
         self.miscData()
 
@@ -67,6 +71,21 @@ class GUI(QWidget):
                                     "border-color: #000000;"
                                     "border-radius :5px")
         mainTLM_Box.resize(500, 400)
+
+    def commandBox(self):
+        command_Box = QLabel('   ', self) # nothing 
+        command_Box.move(12, 487)
+        command_Box.setStyleSheet( "background-color: #FFFFFF;"
+                                    "border-style: solid;"
+                                    "border-width: 1px;"
+                                    "border-color: #000000;"
+                                    "border-radius :5px")
+        command_Box.resize(500, 285)
+
+
+        commandHeader = QLabel('COMMAND VEHICLE', self)
+        commandHeader.move(145, 500)
+        commandHeader.setStyleSheet("background-color: #00FFFFFF;" "font-size: 19pt")
 
     def time_Date(self):
         DatePlaceHolder = QLabel('Date: ', self)
@@ -245,22 +264,23 @@ class GUI(QWidget):
         self.realSIV.move(65, 445)
         self.realSIV.setStyleSheet("background-color: #00FFFFFF;" "font-size: 13pt")
 
-        # code that displays pyro altitudes
-        p1AltLabel = QLabel("PY1 Alt: 200", self)
-        p1AltLabel.move(200, 410)
-        p1AltLabel.setStyleSheet("background-color: #00FFFFFF;" "font-size: 13pt")
+        # code that displays pyro states
+        camStateLabel = QLabel("Cam State: ", self)
+        camStateLabel.move(200, 410)
+        camStateLabel.setStyleSheet("background-color: #00FFFFFF;" "font-size: 13pt")
 
-        p1AltUnit = QLabel("m", self)
-        p1AltUnit.move(295, 410)
-        p1AltUnit.setStyleSheet("background-color: #00FFFFFF;" "font-size: 13pt")
+        self.realcamState = QLabel("0", self)
+        self.realcamState.move(287, 410)
+        self.realcamState.setStyleSheet("background-color: #00FFFFFF;" "font-size: 13pt")
 
-        p2AltLabel = QLabel("PY2 Alt: 0", self)
-        p2AltLabel.move(200, 445)
-        p2AltLabel.setStyleSheet("background-color: #00FFFFFF;" "font-size: 13pt")
+        p2stateLabel = QLabel("Pyro 2 State: ", self)
+        p2stateLabel.move(195, 445)
+        p2stateLabel.setStyleSheet("background-color: #00FFFFFF;" "font-size: 13pt")
 
-        p2AltUnit = QLabel("m", self)
-        p2AltUnit.move(275, 445)
-        p2AltUnit.setStyleSheet("background-color: #00FFFFFF;" "font-size: 13pt")
+        self.realpy2State = QLabel("0", self)
+        self.realpy2State.move(294, 445)
+        self.realpy2State.setStyleSheet("background-color: #00FFFFFF;" "font-size: 13pt")
+
 
         # code that displays GNSS pDOP and Fix
         pdopLabel = QLabel("pDOP:", self)
@@ -279,7 +299,76 @@ class GUI(QWidget):
         self.realfix.move(430, 445)
         self.realfix.setStyleSheet("background-color: #00FFFFFF;" "font-size: 13pt")
     
+    def CommandButtons(self):
+        
+        # camera ON
+        self.CamONbutton =  QPushButton('Cameras ON', self)
+        self.CamONbutton.setStyleSheet("background-color: #50C878;" "font-size: 15pt;" "color: #FFFFFF")
+        self.CamONbutton.resize(160, 50)
+        self.CamONbutton.move(79, 550)
+        
+        self.CamONbutton.clicked.connect(self.sendCAMONSig)
+
+        # camera OFF
+        self.CamOFFbutton =  QPushButton('Cameras OFF', self)
+        self.CamOFFbutton.setStyleSheet("background-color: #6495ED;" "font-size: 15pt;" "color: #FFFFFF")
+        self.CamOFFbutton.resize(160, 50)
+        self.CamOFFbutton.move(280, 550)
+
+        self.CamOFFbutton.clicked.connect(self.sendCAMOFFSig)
+
+        # Pyro 2
+        self.py2Button =  QPushButton(" *FIRE PYRO2* ", self)
+        self.py2Button.setStyleSheet("background-color: #D22B2B;" "font-size: 15pt;" "color: #FFFFFF")
+        self.py2Button.resize(180, 50)
+        self.py2Button.move(59, 620)
+
+        self.py2Button.clicked.connect(self.pyro2ON)
+
+        # Pyro 3
+        self.py3Button =  QPushButton(" *FIRE PYRO3* ", self)
+        self.py3Button.setStyleSheet("background-color: #D22B2B;" "font-size: 15pt;" "color: #FFFFFF")
+        self.py3Button.resize(180, 50)
+        self.py3Button.move(280, 620)
+
+        # SET IDLE
+        self.idleButton =  QPushButton("Set Avionics Launch Detect State", self)
+        self.idleButton.setStyleSheet("background-color: #FFEA00;" "font-size: 15pt;")
+        self.idleButton.resize(365, 50)
+        self.idleButton.move(75, 687)    
+
+        self.idleButton.clicked.connect(self.switchIDLE)
+    
+    def sendCAMONSig(self):
+        hex_value = 0xAB # print this hex value to serial
+        hex_bytes = bytes([hex_value])
+
+        # Send the hexadecimal value to Arduino
+        ser.write(hex_bytes)
+
+    def sendCAMOFFSig(self):
+        hex_value = 0x1C # print this hex value to serial
+        hex_bytes = bytes([hex_value])
+        
+        # Send the hexadecimal value to Arduino
+        ser.write(hex_bytes)
+    
+    def pyro2ON(self):
+        hex_value = 0xD2 # print this hex value to serial
+        hex_bytes = bytes([hex_value])
+        
+        # Send the hexadecimal value to Arduino
+        ser.write(hex_bytes)
+    
+    def switchIDLE(self):
+        hex_value = 0xBC # print this hex value to serial
+        hex_bytes = bytes([hex_value])
+        
+        # Send the hexadecimal value to Arduino
+        ser.write(hex_bytes)
+
     def breakData(self):
+        # ser.open()
         allData = ser.readline()
         allData = allData.decode()
         allData = allData.strip()
@@ -287,11 +376,9 @@ class GUI(QWidget):
         # split data
         allData = str(allData)
         
-        # data packet should follow this order - Acceleration (Z-axis), Vertical Velocity, Flight Computer On Time, Flight Computer Flight Time, Barometric Altitude, Temperature, System State, GPS SIV, GPS PDOP, GPS Fix, GPS Lat, GPS Long, GPS Alt
-        self.Acc, self.Vel, self.OnTme, self.FltTme, self.baroAlt, self.baroTemp, self.state, self.gpsSats, self.gpsPDOP, self.gpsfix, self.gpsLat, self.gpsLon, self.gnssAlt = allData.split(', ')
+        self.Acc, self.Vel, self.OnTme, self.FltTme, self.baroAlt, self.baroTemp, self.state, self.camState, self.py2State, self.gpsSats, self.gpsPDOP, self.gpsfix, self.gpsLat, self.gpsLon, self.gnssAlt = allData.split(', ')
 
     def updateDateTime(self):
-        # using python's datetime library to get the date and time instead of having your GNSS module send it, more bytes for more important data.
         now = datetime.now()
 
         current_time = now.strftime("%H:%M:%S")
@@ -315,6 +402,10 @@ class GUI(QWidget):
         self.realAlt.setText(self.baroAlt)
         self.realTemp.setText(self.baroTemp)
 
+        # pyro states
+        self.realcamState.setText(self.camState)
+        self.realpy2State.setText(self.py2State)
+
         # gnss main
         self.realLat.setText(self.gpsLat)
         self.realLong.setText(self.gpsLon)
@@ -334,15 +425,27 @@ class GUI(QWidget):
             statestr = "INIT"
             color = "#FFC0CB" # hot pink
         elif (int(self.state) == 1):
-            statestr = "IDLE"
-            color = "#90EE90" # green
+            statestr = "CLEAR FLASH"
+            color = "#FFFFE0" # light yellow
         elif (int(self.state) == 2):
-            statestr = "ASCENT"
-            color =  "#ADD8E6" # light blue
+            statestr = "GROUND LOCKED"
+            color = "#FFBF00" # light yellow
         elif (int(self.state) == 3):
+            statestr = "INIT"
+            color = "#90EE90" # green
+        elif (int(self.state) == 4):
+            statestr = "POW FLIGHT"
+            color =  "#ADD8E6" # light blue
+        elif (int(self.state) == 5):
+            statestr = "COAST"
+            color =  "#CBC3E3 " # purple
+        elif (int(self.state) == 6):
             statestr = "DESCENT"
             color = "#FFC933" # yellow
-        elif (int(self.state) == 4 or int(self.state) == 5):
+        elif (int(self.state) == 7):
+            statestr = "UNDER 200m"
+            color = "#FFA500" # orange
+        elif (int(self.state) == 8 or int(self.state) == 9):
             statestr = "LANDED"
             color = "#64EDD2" # teal
 
